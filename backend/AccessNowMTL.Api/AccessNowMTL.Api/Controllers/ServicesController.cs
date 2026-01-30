7782606461fdd98cdd3f44be4d7c1b065d3ec5d4
Ahmad Errekab <Ahmaderrekab@gmail.com>
2026-01-30 16:36:07 -0500

    Add ASP.NET Core backend project

---
+++ backend/AccessNowMTL.Api/AccessNowMTL.Api/Controllers/ServicesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AccessNowMTL.Api.Models;
+
+namespace AccessNowMTL.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ServicesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var services = new[]
+            {
+                new Service { Id = 1, Name = "CLSC", Category = "Health" },
+                new Service { Id = 2, Name = "Housing Help", Category = "Housing" },
+                new Service { Id = 3, Name = "Immigration Help", Category = "Immigration" }
+            };
+
+            return Ok(services);
+        }
+    }
+}
